19699db91145814b0dd6fc667424a2354fa5c948
Anthony Kalsatos <akalsatos@gmail.com>
2014-02-22 20:54:57 -0500

    adding error catching

--- eventHandler.js
+++ eventHandler.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-	$(document).on("ready","#fullPage",function(){
-		function () {
-			var takePicture = document.querySelector("#take-picture");
-		};
+	window.ondevicemotion = function(event) {
+		var x = event.accelerationIncludingGravity.x;
+		return x;
+	}
+	
+	$(document).on("click","#fullPage",function(){
+		if (window.ondevicemotion > 200) {
+			alert("The device can't be oriented in this position";
+		}
+		else {
+			alert("pewpew");
+		}
 	});
 });
